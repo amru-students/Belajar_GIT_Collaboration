a9d95b2f5cb96f50f0fd3cbda24b9854327e6f81
ahmruddin <ahmadamruizzuddin@smkitihsanulfikri.sch.id>
2026-01-28 11:41:42 +0700

    Update Biodata_Amru.py in development branch (2nd)

--- Biodata_Amru.py
+++ Biodata_Amru.py
@@ -6,9 +6,10 @@ nama = 'Ahmad Amru Izzuddin'
 hobi = 'Gambar, Gaming'
 alamat = 'Salaman'
 kota = 'Kab. Magelang'
+provinsi = 'Jawa Tengah'
 
 print(f'Nama Lengkap\t: {nama}')
 print(f'Hobi\t\t: {hobi}')
-print(f'Alamat\t\t: {alamat} yang berada di {kota}')
+print(f'Alamat\t\t: {alamat}, {kota}, {provinsi}')
 
 print('='*63)
