0d09e5cac5583f01677cc83f1d1558aee3de6955
amru-students <ahmadamruizzuddin@smkitihsanulfikri.sch.id>
2026-01-28 11:30:43 +0700

    Add Biodata_Amru.py

---
+++ Biodata_Amru.py
@@ -0,0 +1,14 @@
+print('='*63)
+print('\t\t\tProgram Biodata')
+print('='*63)
+
+nama = 'Ahmad Amru Izzuddin'
+hobi = 'Gambar, Gaming'
+alamat = 'Salaman'
+kota = 'Kab. Magelang'
+
+print(f'Nama Lengkap\t: {nama}')
+print(f'Hobi\t\t: {hobi}')
+print(f'Alamat\t\t: {alamat}, {kota}')
+
+print('='*63)
