7346be1193578b035578dcf61f0113b3cc380bbb
amru-students <ahmadamruizzuddin@smkitihsanulfikri.sch.id>
2026-01-29 09:56:24 +0700

    Add description for this project name 'Keterangan_Repo.py'

---
+++ Keterangan_Repo.py
@@ -0,0 +1,7 @@
+# Ini adalah Tugas Akhir
+
+ket1 = 'Ini dikerjakan pada tanggal 29 Januari 2026 hari Kamis'
+ket2 = 'Ini adalah hasil merge dengan branch training, pun setelah resolve conflict'
+ket3 = 'Ini adalah cherry-pick dari branch training yang di terima oleh branch master'
+
+print(ket1,'\n',ket2,'\t',ket3)
